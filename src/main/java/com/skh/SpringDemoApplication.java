8f92323fe2dc7d2457426ff0a132f79466b4ceb8
kamal <techlearn508@gmail.com>
2025-05-22 21:40:31 +0530

    tc1

--- src/main/java/com/skh/SpringDemoApplication.java
+++ src/main/java/com/skh/SpringDemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoApplication {
+	/*
+	Change1 - test
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
